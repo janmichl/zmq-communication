fae53f8cb4fcbbee8bc346c088a96d230a8d632a
MICHALCZYK Jan <jan.michalczyk@inria.fr>
2017-06-29 15:21:31 +0200

    added msgpack + example with std::vector

--- src/publisher.cpp
+++ src/publisher.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
+#include <sstream>
+#include <time.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <signal.h>
+#include <vector>
 
-#include "zhelpers.hpp"
+#include <msgpack.hpp>
+#include <zmq.hpp>
 
+#define ZEROMQ_NUM_OF_THREADS_USED 1
+
+// this should be templated
 namespace communication
 {
     class Publisher
     {
         public:
-            Publisher() : context_(1),
+            Publisher() : context_(ZEROMQ_NUM_OF_THREADS_USED),
                           publisher_(context_, ZMQ_PUB)
             {
                 //  Prepare our context and publisher
@@ -18,27 +30,82 @@ namespace communication
 
             void publish(const std::string& topic, const std::string& message)
             {
-                std::string full_message = topic + message;
-                s_send(publisher_, full_message.data());
+                sendToTopic(topic.data());
+                send(message.data());
+            }
+            
+            
+            void publish(const std::string& topic, const std::vector<double>& vector)
+            {
+                sendToTopic(topic.data());
+                
+                msgpack::sbuffer sbuf;
+                serializeMessage(sbuf, vector);
+
+                send(sbuf);
             }
         
         
         private:
             zmq::context_t context_;
             zmq::socket_t  publisher_;
+
+
+        private:
+            // this should be templated
+            void serializeMessage(msgpack::sbuffer& sbuf, const std::vector<double>& vector)
+            {
+                // serialize it into simple buffer.
+                msgpack::pack(sbuf, vector);
+            }
+            
+
+            bool send(const msgpack::sbuffer& content)
+            {
+                zmq::message_t message(content.size());
+                memcpy(message.data(), content.data(), content.size());
+                bool rc = publisher_.send(message);
+                
+                return(rc);
+            }
+
+
+            bool send(const std::string& content)
+            {
+                zmq::message_t message(content.size());
+                memcpy(message.data(), content.data(), content.size());
+                bool rc = publisher_.send(message);
+                
+                return(rc);
+            }
+            
+            
+            bool sendToTopic(const std::string& topic_name)
+            {
+                zmq::message_t message(topic_name.size());
+                memcpy(message.data(), topic_name.data(), topic_name.size());
+                bool rc = publisher_.send(message, ZMQ_SNDMORE);
+                
+                return(rc);
+            }
     };
 }
+
 
 int main()
 {
     communication::Publisher publisher;
 
     std::string topic   = "topic";
-    std::string message = "message";
-    while(true)
+    //std::string message = "message";
+    std::vector<double> message;
+    message.push_back(10);
+    message.push_back(20);
+
+    for(;;)
     {
         publisher.publish(topic, message);
     }
     
-    return 0;
+    return(0);
 }
--- src/subscriber.cpp
+++ src/subscriber.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
+#include <sstream>
+#include <time.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <signal.h>
 
-#include "zhelpers.hpp"
+#include <msgpack.hpp>
+#include <zmq.hpp>
+
+#define ZEROMQ_NUM_OF_THREADS_USED 1
 
 namespace communication
 {
     class Subscriber
     {
         public:
-            Subscriber(const std::string& topic) : context_(1),
+            Subscriber(const std::string& topic) : context_(ZEROMQ_NUM_OF_THREADS_USED),
                                                    subscriber_(context_, ZMQ_SUB)
             {
                 //  Prepare our context and subscriber
@@ -19,25 +29,66 @@ namespace communication
 
             std::string receive()
             {
-                return(s_recv(subscriber_));
+                return(receiveFromSocket());
+            }
+            
+            
+            void receive(std::vector<double>& vector)
+            {
+                //remove topic name from message
+                receiveFromSocket();
+
+                msgpack::sbuffer sbuf;
+                receiveFromSocket(sbuf);
+                
+                // deserialize it.
+                msgpack::object_handle oh = msgpack::unpack(sbuf.data(), sbuf.size());
+
+                // print the deserialized object.
+                msgpack::object obj = oh.get();
+                //std::cout << obj << std::endl;
+
+                // convert it into statically typed object.
+                obj.convert(vector);
             }
 
 
         private:
             zmq::context_t context_;
             zmq::socket_t  subscriber_;
+
+
+        private:
+            void receiveFromSocket(msgpack::sbuffer& sbuf)
+            {
+                zmq::message_t message;
+                subscriber_.recv(&message);
+                sbuf.write(static_cast<const char *>(message.data()), message.size());
+            }
+            
+            
+            std::string receiveFromSocket()
+            {
+                zmq::message_t message;
+                subscriber_.recv(&message);
+
+                return(std::string(static_cast<char*>(message.data()), message.size()));
+            }
     };
 }
 
-int main ()
+
+int main()
 {
     communication::Subscriber subscriber(std::string("topic"));
     
+    std::vector<double> message;
     while(true)
     {
-        std::string message = subscriber.receive();
-        std::cout << "Received: " << message << std::endl;
+        //std::string message = subscriber.receive();
+        subscriber.receive(message);
+        std::cout << "Received: " << message[0] << " " << message[1] << std::endl;
     }
     
-    return 0;
+    return(0);
 }
